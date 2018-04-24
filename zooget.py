e9d7931c8621b1b815de41b97045bc2e41900531
Eugene Burachevskiy <eugene.burachevskiy@seavus.com>
2018-04-24 10:43:05 +0300

    initial commit

---
+++ zooget.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import argparse
+from argparse import RawDescriptionHelpFormatter
+from kazoo.client import KazooClient
+import kazoo.exceptions
+
+parser = argparse.ArgumentParser(description='Zookeeper read client.\nReturns zookeeper item data in UTF-8 format to STDOUT. Returns "none" if no item exists', formatter_class=RawDescriptionHelpFormatter)
+parser.add_argument('item', action="store", help='/path/to/item')
+args = parser.parse_args()
+
+zookeeper = {
+    'server':'127.0.0.1',
+    'port':'2181'    
+}
+
+zk = KazooClient(hosts='%(server)s:%(port)s' % zookeeper)
+zk.start()
+
+try:
+    answer = zk.get(args.item)
+    data = answer[0].decode('utf-8')
+    print(data)
+except kazoo.exceptions.NoNodeError:
+    print('none')
+
+zk.stop()
